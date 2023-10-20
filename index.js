00e331d52f24c08d3a3b0bcb734fedb3881c1131
robin-deruyck <90265329+robin-deruyck@users.noreply.github.com>
2023-10-20 23:54:17 +0200

    ci(action): add test step in PR action (#2)
    * refactor: remove unused function
    
    * ci(action): add test step in PR action

--- index.js
+++ index.js
@@ -13,23 +13,6 @@ import {
 } from './src/services/Git.service.js'
 import { isLibInstalled } from './src/services/Environment.service.js'
 
-const loadReleaseConfig = async () => {
-  const configPath = resolveConfigFile(process.argv)
-  if (!configPath) throw new Error(translations.config.fail.invalid_path)
-
-  const jsConfig = await searchFile('**/release.config.{js,cjs}', {
-    ignore: 'node_modules/**'
-  })
-
-  if (!jsConfig) throw new Error(translations.config.fail.file_not_found)
-  return await readJSObjectFromFile(jsConfig)
-}
-
-const checkGitInstalled = async () => {
-  const isGitInstalled = await isLibInstalled('git -v')
-  if (!isGitInstalled) throw new Error(translations.config.fail.git_not_found)
-}
-
 const runSemanticRelease = () => {
   spawn(
     `CI=true npx semantic-release --branches ${getCurrentBranch()} -r ${getLocalRepoUrl()}`,
@@ -42,7 +25,8 @@ const runSemanticRelease = () => {
 
 export const start = async () => {
   try {
-    await checkGitInstalled()
+    // TODO : this check must be made in the bin script
+    // await checkGitInstalled()
     runSemanticRelease()
   } catch (e) {
     Logger.fail(e)
